c13d0b06bff401d7f710e3ad4a431bd25fd7750a
ArrowAi <tanmoyroy1811@gmail.com>
2022-03-13 09:21:23 +0530

    added Comments source code

---
+++ Tutorial/Comments.java
@@ -0,0 +1,26 @@
+/**
+ * Java Program to illustrate comments
+ * @version 1.2 13-03-2022 
+ * @author ArrowA1 
+ */
+
+ public class Comments
+ {
+     public static void main(String[] args)
+     {
+         // these are single line comments
+        System.out.println("Use \"//text goes here\" for single line comments");
+
+         /* These are
+            multiple line comments
+        */
+        System.out.println("Use \"/*text goes here*/\" for multiple line comments");
+
+        /**
+         * These are used 
+         * to generate documentation
+         */
+        System.out.println("Use \"/**text goes here*/\" for documentation");
+     }
+
+ }
