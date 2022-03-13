9b0fd9748cd29e62467731e6791213d845156e71
ArrowAi <tanmoyroy1811@gmail.com>
2022-03-13 08:06:00 +0530

    moved helloworld.java to Tutorial folder

---
+++ Tutorial/helloworld.java
@@ -0,0 +1,24 @@
+// First Java Program
+package Tutorial;
+
+// name of main public class shud correspond with the filename
+// Java is case-sensitive
+public class helloworld
+{   //start of a block
+
+    // execution starts from the main method
+    // hence a main method is required in the source file of class
+    public static void main(String args[])  // main method must be public
+    {
+        System.out.println("Hello World");
+        int s=sum(12,18);
+        System.out.println("Sum of 12 and 18 is "+s);
+    }
+
+    static int sum(int a, int b)
+    {
+        int s=a+b;
+        return s;
+    }
+
+}   // end of the block
